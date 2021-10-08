aaf377221f321a75ad26a42b9c38243be0ad5384
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-08 13:20:03 +0530

    SLL_Pop method.

--- app.js
+++ app.js
@@ -27,11 +27,29 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 let list = new SinglyLinkedList();
 list.push("Sahil");
 list.push("Aujla");
 list.push("g");
-
+console.log(list.pop());
 console.log(list);
