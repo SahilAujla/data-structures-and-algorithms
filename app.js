d15a1821f6ac75b558e5da1a97e5304018faaa10
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-08 12:04:41 +0530

    SLL_Push method.

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-// Also called Bucket Sort
+// A singly linked list if collection of nodes
+// Each node contains a value and address to the next node
 
-function getDigit(num, i) {
-  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
-}
-
-function digitCount(num) {
-  if (num === 0) return 1;
-  return Math.floor(Math.log10(Math.abs(num))) + 1;
-}
-
-function mostDigits(nums) {
-  let maxDigits = 0;
-  for (let i = 0; i < nums.length; i++) {
-    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+// Class for each node
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
-  return maxDigits;
 }
 
-function radixSort(nums) {
-  let maxDigitCount = mostDigits(nums);
-  for (let k = 0; k < maxDigitCount; k++) {
-    let digitBuckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < nums.length; i++) {
-      let digit = getDigit(nums[i], k);
-      digitBuckets[digit].push(nums[i]);
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    nums = [].concat(...digitBuckets);
+    this.length++;
+    return this;
   }
-  return nums;
 }
 
-console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
+let list = new SinglyLinkedList();
+list.push("Sahil");
+list.push("Aujla");
+list.push("g");
+
+console.log(list);
