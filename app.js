da99ef463f7930dffbe3121e8140f91cd675c544
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-10 07:19:53 +0530

    Queue using Singly-Linked List (preferred way).

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
-// Queue using Arrays.
+// Queues using Singly-Linked-List.
+// (can also implement using doubly linked-list which is also preferred way).
 
-let q = [];
-q.push(0);
-q.push(1);
-q.push(2);
-q.shift();
+// here we are removing from the beginning because we need O(1) time complexity and adding to the last.
+
+// Queues --> FIFO principle --> First-in-first-out
+// Stacks --> LIFO principle --> Last-in-first-out
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  dequeue() {
+    if (this.size === 0) return null;
+    let temp = this.first;
+    if (this.size === 1) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  isEmpty() {
+    return !this.size;
+  }
+}
+
+let q = new Queue();
