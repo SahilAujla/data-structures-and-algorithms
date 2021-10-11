66cc1e15786e406ef547de14b994e8eb73485334
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-11 12:37:47 +0530

    BST_Insert method (iteratively).

--- app.js
+++ app.js
@@ -14,27 +14,30 @@ class BinarySearchTree {
   insert(val) {
     let newNode = new Node(val);
     if (!this.root) {
-      this.root = new Node(val);
+      this.root = newNode;
       return this;
     } else {
-      function search(node) {
-        if (val === node.value) return undefined;
-        if (val > node.value) {
-          if (!node.right) {
-            node.right = newNode;
-            return;
+      let current = this.root;
+      while (true) {
+        if (val === current.value) return undefined;
+        if (val < current.value) {
+          if (!current.left) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
           }
-          search(node.right);
         } else {
-          if (!node.left) {
-            node.left = newNode;
-            return;
+          if (val > current.value) {
+            if (!current.right) {
+              current.right = newNode;
+              return this;
+            } else {
+              current = current.right;
+            }
           }
-          search(node.left);
         }
       }
-      search(this.root);
-      console.log(this);
     }
   }
 }
