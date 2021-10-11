54bb64768061560282afd39d959e1ef5d08949cf
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-11 12:30:58 +0530

    BST_insert method (recursively).

--- app.js
+++ app.js
@@ -10,10 +10,33 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = new Node(val);
+      return this;
+    } else {
+      function search(node) {
+        if (val === node.value) return undefined;
+        if (val > node.value) {
+          if (!node.right) {
+            node.right = newNode;
+            return;
+          }
+          search(node.right);
+        } else {
+          if (!node.left) {
+            node.left = newNode;
+            return;
+          }
+          search(node.left);
+        }
+      }
+      search(this.root);
+      console.log(this);
+    }
+  }
 }
 
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
