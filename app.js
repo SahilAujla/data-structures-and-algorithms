4788600e2c14786829b702f893954277276caa92
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-11 14:17:43 +0530

    BST_remove method.

--- app.js
+++ app.js
@@ -60,6 +60,39 @@ class BinarySearchTree {
       }
     }
   }
+
+  remove(val) {
+    if (!this.root) return false;
+
+    if (val === this.root.value) {
+      this.root = null;
+      return true;
+    }
+    let current = this.root;
+    while (true) {
+      if (val > current.value) {
+        if (!current.right) {
+          return false;
+        } else {
+          if (current.right.value === val) {
+            current.right = null;
+            return true;
+          }
+        }
+        current = current.right;
+      } else {
+        if (!current.left) {
+          return false;
+        } else {
+          if (current.left.value === val) {
+            current.left = null;
+            return true;
+          }
+          current = current.left;
+        }
+      }
+    }
+  }
 }
 
 let tree = new BinarySearchTree();
