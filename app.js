2f1b0d8c356ecb9d59d4061705efe5af956e3a57
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-17 16:57:15 +0530

    HashTable Implementation.

--- app.js
+++ app.js
@@ -1,19 +1,88 @@
-function hash(string, arrayLen) {
-  let total = 0;
-  let primeNumber = 31;
-  for (let i = 0; i < Math.min(string.length, 100); i++) {
-    let char = string[i];
-    let value = char.charCodeAt(0) - 96;
-    total = (total * primeNumber + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
+
+  _hash(string) {
+    // here _hash is a private function that's why we put an underscore before its name, although its just for identification and not truly a private function, for making it truly private we can use symbol or weakmaps so it is not accessible on the instances of this class.
+    let total = 0;
+    let primeNumber = 31;
+    for (let i = 0; i < Math.min(string.length, 100); i++) {
+      let char = string[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * primeNumber + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+
+  get(key) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      return undefined;
+    } else {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+  }
+
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
+
+  remove(key) {
+    const index = this._hash(key);
+
+    if (this.keyMap[index] && this.keyMap[index].length) {
+      this.keyMap[index] = [];
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
-// Problems with this hash function
+let ht = new HashTable();
+ht.set("hello", "one");
+ht.set("hi", "two");
+ht.set("hey", "three");
+ht.set("hllo", "four");
+ht.set("hllos", "four");
 
-// 1. works only with strings
-
-// Improvements
-
-// 1. has a time complexity of sort of O(1) because max times the loop runs is 100
-// 2. is a little more random because we used prime numbers
+// if we want to use something like this then we might want to use built in objects and their values in javascript -- this is just for understanding purposes
+// best case time complexity --> O(1)
+// worst case time complexity --> O(n)
