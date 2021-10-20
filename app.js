811636e136b54706eb93fab683cd967dbae98e7f
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-20 15:09:04 +0530

    Dijktra's Algorithm.

--- app.js
+++ app.js
@@ -1,3 +1,114 @@
+// Priority Queue
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+// we are using a min binary heap to implement Priority Queue
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  bubbleUp(index) {
+    let parentIndex = Math.floor((index - 1) / 2);
+    if (parentIndex === -1) return true;
+    while (this.values[parentIndex].priority > this.values[index].priority) {
+      [this.values[parentIndex], this.values[index]] = [
+        this.values[index],
+        this.values[parentIndex],
+      ];
+      index = parentIndex;
+      if (index === 0) return true;
+      parentIndex = Math.floor((index - 1) / 2);
+    }
+    return true;
+  }
+
+  bubbleDown(parentIndex) {
+    while (parentIndex < this.values.length - 1) {
+      let indexOfLeftChild = parentIndex * 2 + 1;
+      let indexOfRightChild = parentIndex * 2 + 2;
+
+      if (this.values[indexOfLeftChild] && this.values[indexOfRightChild]) {
+        if (
+          this.values[indexOfLeftChild].priority <
+            this.values[indexOfRightChild].priority &&
+          this.values[indexOfLeftChild].priority <
+            this.values[parentIndex].priority
+        ) {
+          let childIndex = indexOfLeftChild;
+          let temp = this.values[parentIndex];
+          this.values[parentIndex] = this.values[indexOfLeftChild];
+          this.values[indexOfLeftChild] = temp;
+          parentIndex = childIndex;
+        } else if (
+          this.values[indexOfRightChild].priority <
+            this.values[indexOfLeftChild].priority &&
+          this.values[indexOfRightChild].priority <
+            this.values[parentIndex].priority
+        ) {
+          let childIndex = indexOfRightChild;
+          let temp = this.values[parentIndex];
+          this.values[parentIndex] = this.values[indexOfRightChild];
+          this.values[indexOfRightChild] = temp;
+          parentIndex = childIndex;
+        } else {
+          return true;
+        }
+      } else if (
+        this.values[indexOfLeftChild] &&
+        !this.values[indexOfRightChild]
+      ) {
+        if (
+          this.values[indexOfLeftChild].priority <
+          this.values[parentIndex].priority
+        ) {
+          let childIndex = indexOfLeftChild;
+          let temp = this.values[parentIndex];
+          this.values[parentIndex] = this.values[indexOfLeftChild];
+          this.values[indexOfLeftChild] = temp;
+          parentIndex = childIndex;
+        } else {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    }
+  }
+
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    let index = this.values.length - 1;
+    return this.bubbleUp(index);
+  }
+
+  dequeue() {
+    [this.values[0], this.values[this.values.length - 1]] = [
+      this.values[this.values.length - 1],
+      this.values[0],
+    ];
+    let oldRoot = this.values.pop();
+    this.bubbleDown(0);
+    return oldRoot;
+  }
+}
+
+let qu = new PriorityQueue();
+
+qu.enqueue("Head Shot", 1);
+qu.enqueue("Gunshot Wound", 2);
+qu.enqueue("High Fever", 3);
+qu.enqueue("About to die", 0);
+
+//------------------------------------------------------------------Priority-Queue-End------------------------------------------------------------------------------
+
 class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
@@ -18,6 +129,51 @@ class WeightedGraph {
     if (this.adjacencyList[v2]) {
       this.adjacencyList[v2].push({ node: v1, weight });
     }
+  }
+
+  // Dijktra's Algorithm
+
+  shortestPath(start, end) {
+    let q = new PriorityQueue();
+    let distances = {};
+    let previous = {};
+    let smallest;
+    let path = []; // to return at the end
+    for (let property in this.adjacencyList) {
+      if (property === start) {
+        distances[property] = 0;
+        q.enqueue(property, 0);
+      } else {
+        distances[property] = Infinity;
+        q.enqueue(property, Infinity);
+      }
+      previous[property] = null;
+    }
+
+    while (q.values.length) {
+      smallest = q.dequeue().val;
+      if (smallest === end) {
+        while (previous[smallest]) {
+          path.push(smallest);
+          smallest = previous[smallest];
+        }
+        break;
+      }
+      if (smallest || distances[smallest] != Infinity) {
+        for (let i = 0; i < this.adjacencyList[smallest].length; i++) {
+          let newDistance =
+            distances[smallest] + this.adjacencyList[smallest][i].weight;
+
+          if (newDistance < distances[this.adjacencyList[smallest][i].node]) {
+            distances[this.adjacencyList[smallest][i].node] = newDistance;
+            previous[this.adjacencyList[smallest][i].node] = smallest;
+            q.enqueue(this.adjacencyList[smallest][i].node, newDistance);
+          }
+        }
+      }
+    }
+    console.log(path.concat(smallest).reverse());
+    console.log(distances[end]);
   }
 }
 
