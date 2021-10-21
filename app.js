48ce0b8087cd0a40db30d82d4e1f90c897abc933
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-21 14:08:17 +0530

    Fibonacci using Tabulation.

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-// Fibonacci using Memoization
+// Fibonacci using Tabulation
 
-function fib(n, memo = []) {
-  if (memo[n] !== undefined) return memo[n];
-  if (n === 1 || n === 2) return 1;
-  let res = fib(n - 1, memo) + fib(n - 2, memo);
-  memo[n] = res;
-  return res;
+function fib(n) {
+  if (n === 2 || n === 1) return 1;
+  let fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
 }
 
 let fibs = fib(15);
 console.log(fibs);
 
-// we could have used an object also, instead of an array.
-// The time complexity of this solution is O(n) which is way way better than o(2^n).
-// Memoization is one of the flavours of dynamic programming another flavour is tablulation
-// In memoization we are working top-down but in tabulation we work bottom-up
+// we are working bottom-up here as we are starting to calculate from fib(3) and working our way upto fib(n)
+// by using Tabulation we can save a lot of space as we don't use call stack in this case but in memoization we do
+// so we can also get an error of call stack size exceeded in memoization if the input is very large
+// but using Tabulation we will not get that error
+// Time complexity of this solution is also O(n)
+// but the space complexity of this solution is better than that of memoization solution.
