bae289014ff798dd8ddab06a94226aec94e4bba8
Sahil Aujla <sahilaujla15@gmail.com>
2021-10-21 16:56:43 +0530

    Trie -- all methods except getAllWithPrefix.

--- app.js
+++ app.js
@@ -1,20 +1,116 @@
-// Fibonacci using Tabulation
+// Node
 
-function fib(n) {
-  if (n === 2 || n === 1) return 1;
-  let fibNums = [0, 1, 1];
-  for (let i = 3; i <= n; i++) {
-    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+class Node {
+  constructor() {
+    this.children = {};
+    this.isWord = false;
   }
-  return fibNums[n];
 }
 
-let fibs = fib(15);
-console.log(fibs);
+// Trie
 
-// we are working bottom-up here as we are starting to calculate from fib(3) and working our way upto fib(n)
-// by using Tabulation we can save a lot of space as we don't use call stack in this case but in memoization we do
-// so we can also get an error of call stack size exceeded in memoization if the input is very large
-// but using Tabulation we will not get that error
-// Time complexity of this solution is also O(n)
-// but the space complexity of this solution is better than that of memoization solution.
+class Trie {
+  constructor() {
+    this.root = new Node();
+    // this.output = [];
+  }
+
+  insert(word) {
+    let current = this.root;
+    for (let i = 0; i < word.length; i++) {
+      let charToInsert = word[i];
+      if (!(charToInsert in current.children)) {
+        current.children[charToInsert] = new Node();
+      }
+
+      current = current.children[charToInsert];
+    }
+
+    current.isWordEnd = true;
+  }
+
+  contains(word) {
+    let current = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      let charToFind = word[i];
+      if (!(charToFind in current.children)) {
+        return false;
+      }
+
+      current = current.children[charToFind];
+    }
+
+    return current.isWordEnd;
+  }
+
+  startsWith(prefix) {
+    let current = this.root;
+
+    for (let i = 0; i < prefix.length; i++) {
+      let charToFind = prefix[i];
+      if (!(charToFind in current.children)) {
+        return false;
+      }
+
+      current = current.children[charToFind];
+    }
+
+    return true;
+  }
+
+  remove(word) {
+    let current = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      let charToFind = word[i];
+      if (!(charToFind in current.children)) {
+        console.log(`The word '${word}' did not exist in the trie `);
+        return;
+      }
+
+      current = current.children[charToFind];
+    }
+    current.isWordEnd = false;
+  }
+
+  // getAllWithPrefix(prefix) {
+  //   let current = this.root;
+
+  //   for (let i = 0; i < prefix.length; i++) {
+  //     let charToFind = prefix[i];
+  //     if (!(charToFind in current.children)) {
+  //       console.log(`No word starts with the prefix '${prefix}' in the trie`);
+  //       return;
+  //     }
+
+  //     current = current.children[charToFind];
+  //   }
+  //   this.traverse(current);
+  // }
+
+  // traverse(node) {
+  //   let children = [];
+  //   for (let child in node.children) {
+  //     node.children[child].name = child;
+  //     children.push(node.children[child]);
+  //   }
+  //   let pushElement = "";
+  //   for (let i = 0; i < children.length; i++) {
+  //     pushElement += children[i].name;
+  //     if (children[i].isWordEnd) {
+  //       this.output.push(pushElement);
+  //     }
+  //   }
+
+  //   for (let i = 0; i < children.length; i++) {
+  //     this.traverse(children[i]);
+  //   }
+  //   console.log(this.output);
+  // }
+}
+
+let tr = new Trie();
+tr.insert("Sahil");
+tr.insert("Aujla");
+tr.insert("Sahgk");
